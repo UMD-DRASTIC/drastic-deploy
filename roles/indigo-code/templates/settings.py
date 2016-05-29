6264931ce50e4a132a60803577b4fcab9a75a553
Kaldrill <jerome.fuselier@free.fr>
2016-05-29 21:42:04 +0200

    Remove settings vriable

--- roles/indigo-code/templates/settings.py
+++ roles/indigo-code/templates/settings.py
@@ -1,4 +1,3 @@
 KEYSPACE = 'indigo'
 LOG_LEVEL = 'INFO'
 CASSANDRA_HOSTS = ("{{ hostvars[inventory_hostname]['ansible_' ~ cassandra_interface]['ipv4']['address'] }}", )
-REPLICATION_FACTOR = {{ cassandra_replication_factor }}
