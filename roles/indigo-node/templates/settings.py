439da9d3810ea6421ee37f39299a7f1892b95281
Gregory Jansen <gregory.jansen@gmail.com>
2016-08-04 15:36:26 -0400

    removed legacy config file inlined the bitbucket key, instead of separate file (comes from separate inventory) added a local python 2.7.10 install to handle cassandra-driver incompatibility with python 2.7.11+ avoided the bundled CQL client on cassandra nodes (same reason as above), see indigo-env

--- roles/indigo-node/templates/settings.py
+++ roles/indigo-node/templates/settings.py
@@ -1,4 +1,5 @@
 KEYSPACE = 'indigo'
 LOG_LEVEL = 'INFO'
-CASSANDRA_HOSTS = ("{{ hostvars[inventory_hostname]['ansible_' ~ cassandra_interface]['ipv4']['address'] }}", )
+CASSANDRA_HOSTS = [ {% for item in groups['indigo-databases'] %}"{{ hostvars[item]['ansible_' ~ hostvars[item]['cassandra_interface']]['ipv4']['address'] }}",{% endfor %} ]
 REPLICATION_FACTOR = {{ cassandra_replication_factor }}
+CONSISTENCY_LEVEL = {{ groups['indigo-databases']|length - cassandra_replication_factor + 1 }}
