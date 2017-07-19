d21c4b3fafbec01d7982fe491f8a2a8cef373894
Gregory Jansen <gregory.jansen@gmail.com>
2017-07-19 13:48:14 -0400

    remove graph schema clear from DSE init

--- roles/cassandra_archive/files/mydse-init.groovy
+++ roles/cassandra_archive/files/mydse-init.groovy
@@ -5,7 +5,6 @@ import com.datastax.bdp.graph.api.query.Search
 :remote console
 system.graph('drasticgraph').replication("{'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1}").systemReplication("{'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1}").option("graph.schema_mode").set("Development").option("graph.allow_scan").set("true").option("graph.default_property_key_cardinality").set("multiple").option("graph.tx_groups.*.write_consistency").set("ONE").ifNotExists().create()
 :remote config alias g drasticgraph.g
-schema.clear()
 schema.propertyKey("URI").Text().single().create()
 schema.propertyKey("type").Text().single().create()
 schema.propertyKey("value").Text().single().create()
